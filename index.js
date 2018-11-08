87df243820f8b0e56d37959d4982991b14b4e3a4
ayylmar <34417135+ayylmar@users.noreply.github.com>
2018-11-08 23:35:31 +0100

    Use remote addresses for world wrappers

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function makeOpts(name) {
 }
 
 const getLocalAddress = which => Config[`LOCAL_WORLD${which}_HOST`] +
-  ":" + Config[`LOCAL_WORLD${which}_PORT`];
+  ':' + Config[`LOCAL_WORLD${which}_PORT`];
 
 function loginCb(name, event) {
   switch (name) {
@@ -23,7 +23,7 @@ function loginCb(name, event) {
       }
       return true;
     case 'S_SELECT_WORLD':
-      this.emit('S_SELECT_WORLD');
+      this.emit('S_SELECT_WORLD', [event.worldIP1, event.worldIP2]);
       event.worldIP1 = getLocalAddress(1);
       event.worldIP2 = getLocalAddress(2);
       return true;
@@ -38,19 +38,24 @@ function world2Cb(name, event) {
   
 }
 
-function createWorldWrappers() {
-  const world1Wrapper = new Wrapper({
-    name: 'World1', 
-    options: makeOpts('WORLD1'),
-    dispatch: world1Cb,
-    isWorld: true
-  });
-  const world2Wrapper = new Wrapper({
-    name: 'World2', 
-    options: makeOpts('WORLD2'),
-    dispatch: world2Cb,
-    isWorld: true
-  });
+const worldCallbacks = [world1Cb, world2Cb];
+
+function createWorldWrappers(remoteWorlds) {
+  const worldWrappers = [];
+  for (let i = 0, len = remoteWorlds.length; i < len; ) {
+    const [host, port] = remoteWorlds[i].split(":");
+    const cb = worldCallbacks[i++];
+    worldWrappers.push(new Wrapper({
+      name: 'World' + i, 
+      options: {
+        ...makeOpts('WORLD' + i),
+        remoteHost: host,
+        remotePort: port | 0,
+      },
+      dispatch: cb,
+      isWorld: true
+    }));
+  }
 }
 
 const loginWrapper = new Wrapper({
